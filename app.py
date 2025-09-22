eec842aa0a74b2433c9d022549d79ff0cdcde2ca
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-22 14:18:19 +0000

    Import pages 3,4,5 to file and register them.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from multipage import MultiPage
-from app_pages import ProjectSummary
-from app_pages import AnimalVisualizer
+from app_pages import ProjectSummary, AnimalVisualizer, AnimalDetection, HypothesesValidation, MLMetrics
+
 
 # Create app instance
 app = MultiPage("Animal Detection App")
@@ -9,6 +9,9 @@ app = MultiPage("Animal Detection App")
 # Register pages
 app.app_page("Project Summary", ProjectSummary.page1_body)
 app.app_page("Animal Visualizer", AnimalVisualizer.page2_body)
+app.app_page("Animal Detection App", AnimalDetection.page3_body)
+app.app_page("Project Hypotheses and Validation", HypothesesValidation.page4_body)
+app.app_page("ML Metrics", MLMetrics.page5_body)
 
 # Run the app
 if __name__ == "__main__":
