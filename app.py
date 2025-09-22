8f9f5b7909c22537e3bd37d21f0895bf6a54774d
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-22 14:05:43 +0000

    Add page 2 file path and register new page on dashboard.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from multipage import MultiPage
 from app_pages import ProjectSummary
+from app_pages import AnimalVisualizer
 
 # Create app instance
 app = MultiPage("Animal Detection App")
 
 # Register pages
-app.app_page("Quick Project Summary", ProjectSummary.page1_body)
+app.app_page("Project Summary", ProjectSummary.page1_body)
+app.app_page("Animal Visualizer", AnimalVisualizer.page2_body)
 
 # Run the app
 if __name__ == "__main__":
