5cc77b6d765639f7e6df25bb8b526b1fac7dfacb
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-23 14:22:32 +0000

    New function to create a matplotlib figure.

--- src/visualization.py
+++ src/visualization.py
@@ -22,3 +22,29 @@ def compute_variability_image(filepaths):
     std_img = np.std(imgs, axis=0)
 
     return std_img
+
+def plot_mean_variability_for_classes(df, classes, n_samples=20):
+    """
+    Create a matplotlib plot.
+    Shows avg & std images for given classes.
+    """
+    fig, axes = plt.subplots(len(classes), 2, figsize=(8, 4*len(classes)))
+
+    if len(classes) == 1:
+        axes = [axes]  # ensure iterable
+
+    for i, cls in enumerate(classes):
+        class_files = df[df['label'] == cls]['filepath'].sample(min(n_samples, len(df)))
+        avg_img = compute_average_image(class_files)
+        std_img = compute_variability_image(class_files)
+
+        axes[i][0].imshow(avg_img, cmap="gray")
+        axes[i][0].set_title(f"{cls} - Average")
+        axes[i][0].axis("off")
+
+        axes[i][1].imshow(std_img, cmap="gray")
+        axes[i][1].set_title(f"{cls} - Variability")
+        axes[i][1].axis("off")
+
+    plt.tight_layout()
+    return fig
