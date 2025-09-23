a80277f6f3077a5c6e378ba87d7b4f815637cb74
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-23 14:18:20 +0000

    New function computes average image for list of filepaths.

--- src/visualization.py
+++ src/visualization.py
@@ -2,3 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from PIL import Image
+
+
+def compute_average_image(filepaths):
+    """
+    Compute the average image for a list of filepaths.
+    Returns numpy array.
+    """
+    imgs = [np.array(Image.open(fp).convert("L")) for fp in filepaths]
+    avg_img = np.mean(imgs, axis=0)
+
+    return avg_img
