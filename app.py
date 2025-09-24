8ffdc38484a388c2e98f2f7df48acf1e4d4ba194
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-24 14:58:13 +0000

    Add hypotheses & validation page paths.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from app_pages.ProjectSummary import page_project_summary
 from app_pages.AnimalVisualizer import page_animal_visualizer
 from app_pages.AnimalDetection import page_animal_detection
+from app_pages.HypothesesValidation import page_hypotheses_validation
 
 
 df = pd.read_csv("inputs/datasets/animal_predictions.csv")
@@ -13,7 +14,8 @@ def main():
     page = st.sidebar.radio("Go to", [
         "Project Summary",
         "Animal Visualizer",
-        "Animal Detection"
+        "Animal Detection",
+        "Hypotheses and Validation"
         ])
 
     if page == "Project Summary":
@@ -24,6 +26,10 @@ def main():
     
     elif page == "Animal Detection":
         page_animal_detection(df)
+    
+    elif page == "Hypotheses and Validation":
+        page_hypotheses_validation()
+
 
 if __name__ == "__main__":
     main()
