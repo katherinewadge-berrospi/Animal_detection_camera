e87aacb17674ef5bed1141d8cab8bcc6662f3026
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-24 13:40:21 +0000

    Add functioning animal detection page to sidebar.

--- app.py
+++ app.py
@@ -3,19 +3,27 @@ import pandas as pd
 
 from app_pages.ProjectSummary import page_project_summary
 from app_pages.AnimalVisualizer import page_animal_visualizer
+from app_pages.AnimalDetection import page_animal_detection
 
 
 df = pd.read_csv("inputs/datasets/animal_predictions.csv")
 
 def main():
     st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ["Project Summary", "Animal Visualizer"])
+    page = st.sidebar.radio("Go to", [
+        "Project Summary",
+        "Animal Visualizer",
+        "Animal Detection"
+        ])
 
     if page == "Project Summary":
         page_project_summary()
 
     elif page == "Animal Visualizer":
         page_animal_visualizer(df)
+    
+    elif page == "Animal Detection":
+        page_animal_detection(df)
 
 if __name__ == "__main__":
     main()
