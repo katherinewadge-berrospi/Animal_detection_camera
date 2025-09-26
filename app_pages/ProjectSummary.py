055d357afefb39eb8b8906605ccf2ea35fb016d0
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-26 10:33:58 +0000

    Improve text content for final page version.

--- app_pages/ProjectSummary.py
+++ app_pages/ProjectSummary.py
@@ -8,32 +8,47 @@ def page_project_summary():
     st.subheader("General Information")
     st.write(
         """
-        The project focuses on automatic detection of animal species from images. 
-        The aim is to support ecological research, wildlife monitoring, and conservation efforts 
-        by classifying animals accurately using computer vision techniques.
+        The project focuses on automatic detection of animal species from 
+        images for our client, Steve B. The aim is to support ecological 
+        research, wildlife monitoring, and conservation efforts by classifying 
+        animals accurately using computer prediction techniques.
         """
     )
 
     st.subheader("Project Dataset")
     st.write(
         """
-        The dataset contains thousands of animal images across 64 species 
-        (including, but not exclusive to, antelope, beaver, chimpanzee, and dolphin). 
-        Images are split into training, validation, and test sets.
+        The dataset contains 14.7k images of animal images across 64 species,
+        sourced from Kaggle (including, but not exclusive to, antelope, beaver, 
+        chimpanzee, and dolphin). 
+        Images were pre-processed, augmented, and split into training, 
+        validation, and test sets.
         """
     )
     st.markdown(
-        "[Project README](../README.md)",
+        "[Project README]"
+        "(https://github.com/katherinewadge-berrospi/Animal_detection_camera/blob/main/README.md)",
         unsafe_allow_html=True
     )
 
     st.subheader("Business Requirements")
     st.write(
         """
-        - The client is interested in exploring visual differences between species 
-        (e.g., variability and average features per class).  
-        - The client is interested in an application that predicts which animal is 
-        present in an uploaded image.
+        1. Can the system highlight visual differences between species (e.g., 
+        average features, variability)?  
+        2. Can the system predict which animal species is present in an uploaded 
+        image with high accuracy?
         """
     )
     
+    st.subheader("Hypotheses")
+    st.write(
+        """
+        - **Hypothesis 1**: The model can differentiate between animal species
+        based on visual features such as colour and shape.
+        - **Hypothesis 2**: Training the model on a smaller dataset reduces model
+        performance and accuracy.
+        - **Hypothesis 3**: Training the model on non-augmented images will reduce
+        model performance and accuracy.
+        """
+    )
