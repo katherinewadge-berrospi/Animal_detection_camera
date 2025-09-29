da67c060050a13185133c13270f2bf6548b0398e
katherinewadge-berrospi <katherinewadge@gmail.com>
2025-09-29 15:42:16 +0000

    Convert final_model.h5 into tflite.

--- convert_model/convert_model.py
+++ convert_model/convert_model.py
@@ -1,20 +1,21 @@
 import tensorflow as tf
+import os
 
+MODEL_PATH = os.path.join("outputs", "final_model.h5")
+OUT_PATH = os.path.join("app_artifacts", "final_model.tflite")
 
-model = tf.keras.models.load_model("app_artifacts/final_model.h5")
+model = tf.keras.models.load_model(MODEL_PATH)
 
 # Convert to TFLite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.target_spec.supported_ops = [
-    tf.lite.OpsSet.TFLITE_BUILTINS,
-    tf.lite.OpsSet.SELECT_TF_OPS
-]
+    tf.lite.OpsSet.TFLITE_BUILTINS,]
 
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 
 # Save the model
-with open("app_artifacts/final_model.tflite", "wb") as f:
+with open(OUT_PATH, "wb") as f:
     f.write(tflite_model)
 
-print("Conversion complete! Saved as app_artifacts/final_model.tflite")
+print(f"Conversion complete! Saved as {OUT_PATH}")
